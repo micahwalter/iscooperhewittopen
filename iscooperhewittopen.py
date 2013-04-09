623669e21b503fff35d6f5e89cd7a93db3391a88
Micah Walter <micah@micahwalter.com>
2013-04-08 22:51:12 -0400

    styles

--- iscooperhewittopen.py
+++ iscooperhewittopen.py
@@ -12,9 +12,9 @@ app = Flask(__name__)
 @app.route('/')
 def iscooperhewittopen():
 	if checkStatus() != '0':
-		return "YES!"
+		return weOpen
 	else:
-		return "NO!"
+		return weClosed
 
 
 def checkStatus():
@@ -41,3 +41,73 @@ def checkStatus():
 	status = str(status)
 	
 	return status
+
+weOpen = """
+<!DOCTYPE html>
+<!--[if IE]><![endif]-->
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="Is Cooper-Hewitt Open? Find out here." />
+    <meta name="application-name" content="Is Cooper-Hewitt Open?" />
+    <meta name="msapplication-tooltip" content="Is Cooper-Hewitt Open?" />
+    <meta name="author" content="Micah Walter" />
+    <meta http-equiv="imagetoolbar" content="false" />
+
+
+    <title>Is Cooper-Hewitt Open?</title>
+
+    <link href="static/style.css" media="all" rel="stylesheet" type="text/css" />
+    <!--[if lt IE 9]>
+        <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <header>
+      <h1>Is Cooper-Hewitt Open?</h1>
+    </header>
+    <section id="status">
+      <h2>
+        YUP!
+      </h2>
+    </section>
+  </body>
+</html>
+"""
+
+weClosed = """
+<!DOCTYPE html>
+<!--[if IE]><![endif]-->
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="Is Cooper-Hewitt Open? Find out here." />
+    <meta name="application-name" content="Is Cooper-Hewitt Open?" />
+    <meta name="msapplication-tooltip" content="Is Cooper-Hewitt Open?" />
+    <meta name="author" content="Micah Walter" />
+    <meta http-equiv="imagetoolbar" content="false" />
+
+
+    <title>Is Cooper-Hewitt Open?</title>
+
+    <link href="static/style.css" media="all" rel="stylesheet" type="text/css" />
+    <!--[if lt IE 9]>
+        <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <header>
+      <h1>Is Cooper-Hewitt Open?</h1>
+    </header>
+    <section id="status">
+      <h2>
+        NOPE!
+      </h2>
+    </section>
+  </body>
+</html>
+"""
